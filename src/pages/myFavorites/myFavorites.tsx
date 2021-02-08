f1e5540733e7ebc5450615b8df49dd71892e06c5
Mathieu Enault <me94110@gmail.com>
2021-02-08 21:07:14 +0100

    end of the favorites for modal card with data onclick

--- src/pages/myFavorites/myFavorites.tsx
+++ src/pages/myFavorites/myFavorites.tsx
@@ -9,30 +9,42 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
-import { RootRef } from "@material-ui/core";
-import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
-//import Modal from "@material-ui/core/Modal";
-import Backdrop from "@material-ui/core/Backdrop";
-import Fade from "@material-ui/core/Fade";
+import { DELETE_CRYPTO } from "../../redux/Actions/AddAndDeleteActions";
 import Modal from "react-modal";
-import ReactDOM from "react-dom";
-/////////////
-const customStyles = {
-  content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-  },
-};
 
-//////////////////////
+///////////// styles
 
+/////////
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
+    breakpoints: {
+      values: {
+        xs: 0,
+        sm: 600,
+        md: 960,
+        lg: 1280,
+        xl: 1920,
+      },
+    },
+    modalParent: {},
+    modal: {
+      width: "100%",
+      height: "100%",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    modalContent: {
+      width: "300px",
+      height: "600px",
+      margin: "auto",
+      border: "none",
+      [theme.breakpoints.up("md")]: {
+        width: "600px",
+      },
+    },
     root: {
       maxWidth: 200,
       margin: "auto",
@@ -56,43 +68,55 @@ const useStyles = makeStyles((theme: Theme) =>
       borderRadius: "50%",
       textAlign: "center",
       fontWeight: 600,
+      margin: "0 auto 15px auto",
+      [theme.breakpoints.up("md")]: {
+        margin: "0",
+      },
     },
     datas_keys: {
       fontWeight: 600,
-    }, //modal popup
-    modal: {
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
     },
+
     paper: {
       backgroundColor: theme.palette.background.paper,
       border: "2px solid #000",
       boxShadow: theme.shadows[5],
       padding: theme.spacing(2, 4, 3),
     }, //
+    cryptoTitle: {
+      alignItems: "center",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+    },
+    dataTitle: {
+      marginBottom: "10px",
+    },
+    dataLine: {
+      display: "flex",
+      justifyContent: "space-between",
+    },
+    datasKeys: {
+      fontWeight: 600,
+    },
+    detailsDatas: {
+      right: 0,
+      color: "#000",
+      fontWeight: 600,
+    },
+    detailsDatasImage: {
+      margin: "10px 0",
+    },
   })
 );
+
+/////////////
+
 const myFavorites: FC = () => {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
-  const [cryptoDatas, setCryptosDatas] = React.useState([]);
   const [selectedFav, setSelectedFav] = React.useState(null);
-  var subtitle;
   const [modalIsOpen, setIsOpen] = React.useState(false);
-  function openModal(fav) {
-    setIsOpen(true);
-    setSelectedFav(fav);
-  }
 
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    subtitle.style.color = "#f00";
-  }
-
-  function closeModal() {
-    setIsOpen(false);
-  }
   // Récupération du redux storage
   const Favorites = useSelector(
     (state: RootStore) => state.favorites.favoriteDatas
@@ -101,30 +125,31 @@ const myFavorites: FC = () => {
   const newArray = [...Favorites];
   console.log(newArray);
 
+  //Modal
+  function openModal(fav) {
+    setIsOpen(true);
+    setSelectedFav(fav);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   ///////////// Modals events ////
-  const rootRef = React.useRef<HTMLDivElement>(Favorites);
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-  //////////////////////////////////
-
-  //const refs = useRef(Object.keys(Favorites).map((data) => React.createRef([data])) )
-  //console.log(refs)
-  /*
-const tentRefs = useRef<(HTMLDivElement | null)[]>([])
-const test = Favorites.map(index=> tentRefs.current.push(index) )
-console.log(test);
-console.log(tentRefs)
-console.log(typeof(refs))
-Object.keys(Favorites).map(data => console.log(Favorites[data]));
-const tentRefs = useRef<(HTMLDivElement | null)[]>([])*/
-
+  //index modal
+  let ctyptoId;
+  //index favorites
   let index = 1;
+  //formatedDate
+
+  const ChangeFormateDate = (date) => {
+    return (
+      date.substring(0, 10).toString().split("-").reverse().join("-") +
+      " à " +
+      date.substring(11, 16).replace(":"[0], "h")
+    );
+  };
+  //
   return (
     <div style={{}}>
       <h1 className={classes.header}> Favoris</h1>
@@ -193,23 +218,241 @@ const tentRefs = useRef<(HTMLDivElement | null)[]>([])*/
       <div>
         <Modal
           isOpen={modalIsOpen}
-          onAfterOpen={afterOpenModal}
           onRequestClose={closeModal}
-          style={customStyles}
+          className={classes.modal}
           contentLabel="Example Modal"
         >
-          <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
-          <button onClick={closeModal}>close</button>
-          {selectedFav && (
-            <div>
-              <span className={classes.datas_keys}>Rang :</span>{" "}
-              {selectedFav["0"].market_cap_rank}
-            </div>
-          )}
+          <h2
+            ref={(_ctyptoId) => (ctyptoId = _ctyptoId)}
+            onClick={closeModal}
+            className={classes.modalContent}
+          >
+            {selectedFav && (
+              <div className={classes.paper}>
+                <div className={classes.cryptoTitle}>
+                  <h2 id="transition-modal-title">
+                    <span className={classes.dataTitle}>Détails du </span>{" "}
+                    <span className={classes.detailsDatas}>
+                      {selectedFav["0"].name}
+                    </span>
+                  </h2>
+                  <li className={classes.dataLine}>
+                    <span className={classes.datasKeys}>En date du :</span>
+                    <span className={classes.detailsDatas}>
+                      {" "}
+                      {ChangeFormateDate(selectedFav["0"].last_updated)}
+                    </span>
+                  </li>
+                  <img
+                    className={classes.detailsDatasImage}
+                    src={selectedFav["0"].image}
+                    width={80}
+                    height={80}
+                    alt="nom de cryptomonnaie"
+                  />
+                </div>
+                <ul id="transition-modal-description">
+                  <li className={classes.dataLine}>
+                    <span className={classes.datasKeys}>Symbole :</span>
+                    <span className={classes.detailsDatas}>
+                      {" "}
+                      {selectedFav["0"].symbol.toUpperCase()}
+                    </span>
+                  </li>
+                  <ul>
+                    <li className={classes.dataLine}>
+                      <span className={classes.datasKeys}>Rang :</span>
+                      <span className={classes.detailsDatas}>
+                        {selectedFav["0"].market_cap_rank}
+                      </span>
+                    </li>
+                    <li className={classes.dataLine}>
+                      <span className={classes.datasKeys}>
+                        En valeur (24h) :
+                      </span>
+                      <span className={classes.detailsDatas}>
+                        {selectedFav["0"].market_cap_change_24h} €
+                      </span>
+                    </li>
+                    <li className={classes.dataLine}>
+                      <span className={classes.datasKeys}>
+                        En pourcentage (24h) :
+                      </span>
+                      <span className={classes.detailsDatas}>
+                        {selectedFav[
+                          "0"
+                        ].market_cap_change_percentage_24h.toFixed(2)}{" "}
+                        %
+                      </span>
+                    </li>
+                  </ul>
+                  <li>
+                    <span className={classes.datasKeys}>
+                      Record historique ( max ) :
+                    </span>
+                    <ul>
+                      <li className={classes.dataLine}>
+                        <span className={classes.datasKeys}>En valeur :</span>{" "}
+                        <span className={classes.detailsDatas}>
+                          {" "}
+                          {selectedFav["0"].ath} ‎€
+                        </span>
+                      </li>
+
+                      <li className={classes.dataLine}>
+                        <span className={classes.datasKeys}>
+                          En pourcentage :
+                        </span>
+                        <span className={classes.detailsDatas}>
+                          {" "}
+                          {selectedFav["0"].ath_change_percentage.toFixed(2)} %
+                        </span>
+                      </li>
+                      <li style={{}} className={classes.dataLine}>
+                        <span className={classes.datasKeys}>Date :</span>{" "}
+                        <span className={classes.detailsDatas}>
+                          {" "}
+                          {ChangeFormateDate(selectedFav["0"].ath_date)}
+                        </span>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <span className={classes.datasKeys}>
+                      Record historique ( min ) :
+                    </span>
+                    <ul>
+                      <li className={classes.dataLine}>
+                        <span className={classes.datasKeys}>En valeur :</span>{" "}
+                        <span className={classes.detailsDatas}>
+                          {" "}
+                          {selectedFav["0"].atl} ‎€
+                        </span>
+                      </li>
+
+                      <li className={classes.dataLine}>
+                        <span className={classes.datasKeys}>
+                          En pourcentage :
+                        </span>
+                        <span className={classes.detailsDatas}>
+                          {" "}
+                          {selectedFav["0"].atl_change_percentage.toFixed(2)} %
+                        </span>
+                      </li>
+                      <li className={classes.dataLine}>
+                        <span className={classes.datasKeys}>Date :</span>{" "}
+                        <span className={classes.detailsDatas}>
+                          {" "}
+                          {ChangeFormateDate(selectedFav["0"].atl_date)}
+                        </span>
+                      </li>
+                    </ul>
+                  </li>
+                  <li className={classes.dataLine}>
+                    {" "}
+                    <span className={classes.datasKeys}>En circulation :</span>
+                    <span className={classes.detailsDatas}>
+                      {" "}
+                      {selectedFav["0"].circulating_supply} tokens
+                    </span>
+                  </li>
+                  <li className={classes.dataLine}>
+                    <span className={classes.datasKeys}>Prix actuel :</span>{" "}
+                    <span className={classes.detailsDatas}>
+                      {" "}
+                      {selectedFav["0"].current_price} ‎€
+                    </span>
+                  </li>
+                  <li className={classes.dataLine}>
+                    <span className={classes.datasKeys}>Maximum en 24h :</span>{" "}
+                    <span className={classes.detailsDatas}>
+                      {" "}
+                      {selectedFav["0"].high_24h} ‎€
+                    </span>
+                  </li>
+
+                  <li className={classes.dataLine}>
+                    <span className={classes.datasKeys}>Minimum en 24h :</span>
+                    <span className={classes.detailsDatas}>
+                      {" "}
+                      {selectedFav["0"].low_24h}
+                    </span>
+                  </li>
+                  <li>
+                    <li className={classes.dataLine}>
+                      <span className={classes.datasKeys}>
+                        Capitalisation boursière :
+                      </span>
+                      <span className={classes.detailsDatas}>
+                        {" "}
+                        {selectedFav["0"].market_cap}
+                      </span>
+                    </li>
+                  </li>
+                  <li className={classes.dataLine}>
+                    <span className={classes.datasKeys}>Offre maximale :</span>
+                    <span className={classes.detailsDatas}>
+                      {selectedFav["0"].max_supply} tokens
+                    </span>
+                  </li>
+                  <li>
+                    <span className={classes.datasKeys}>
+                      Évolution en 24h :
+                    </span>
+                    <ul>
+                      <li className={classes.dataLine}>
+                        <span className={classes.datasKeys}>En valeur :</span>{" "}
+                        <span className={classes.detailsDatas}>
+                          {" "}
+                          {selectedFav["0"].price_change_24h} €
+                        </span>
+                      </li>
+                      <li className={classes.dataLine}>
+                        <span className={classes.datasKeys}>
+                          En pourcentage :
+                        </span>
+                        <span className={classes.detailsDatas}>
+                          {selectedFav["0"].price_change_percentage_24h.toFixed(
+                            2
+                          )}{" "}
+                          %
+                        </span>
+                      </li>
+                    </ul>
+                  </li>
+
+                  <li className={classes.dataLine}>
+                    <span className={classes.datasKeys}>Offre totale :</span>
+                    <span className={classes.detailsDatas}>
+                      {" "}
+                      {selectedFav["0"].total_supply} tokens
+                    </span>
+                  </li>
+                  <li className={classes.dataLine}>
+                    <span className={classes.datasKeys}>Volume total :</span>
+                    <span className={classes.detailsDatas}>
+                      {" "}
+                      {selectedFav["0"].total_volume}
+                    </span>
+                  </li>
+                  <li className={classes.dataLine}>
+                    <span className={classes.datasKeys}>Roi :</span>{" "}
+                    <span className={classes.detailsDatas}>
+                      {" "}
+                      {selectedFav["0"].roi ? (
+                        <li>{fav["0"].market_cap_rank}</li>
+                      ) : (
+                        "Pas de données"
+                      )}
+                    </span>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </h2>
         </Modal>
       </div>
     </div>
   );
 };
-
 export default myFavorites;
--- src/redux/Reducers/favoritesReducer.ts
+++ src/redux/Reducers/favoritesReducer.ts
@@ -1,28 +1,31 @@
-
-import {FavoritesCryptoState,ADD_CRYPTO,DELETE_CRYPTO,FavoritesCryptoData, FavoritesCryptoTypes } from "../Types/searchCryptoTypes";
-
+import {
+  FavoritesCryptoState,
+  ADD_CRYPTO,
+  DELETE_CRYPTO,
+  FavoritesCryptoData,
+  FavoritesCryptoTypes,
+} from "../Types/searchCryptoTypes";
 
 const defaultState: FavoritesCryptoState = {
   favoriteDatas: [],
-  position : 0
+  position: 0,
 };
 
-
-
-const favoritesCryptoReducer = (state = defaultState, action: FavoritesCryptoTypes ): FavoritesCryptoState => {
-  
-    switch (action.type) {
-     case ADD_CRYPTO:
-         
+const favoritesCryptoReducer = (
+  state = defaultState,
+  action: FavoritesCryptoTypes
+): FavoritesCryptoState => {
+  switch (action.type) {
+    case ADD_CRYPTO:
       return {
-          favoriteDatas: [...state.favoriteDatas, {...action.payload }]
-      }
-  
+        favoriteDatas: [...state.favoriteDatas, { ...action.payload }],
+      };
+    /*  case DELETE_CRYPTO:
+      return state.filter((favorites) => favorites.id != action.payload);*/
+
     default:
-      return state
+      return state;
   }
 };
 
 export default favoritesCryptoReducer;
-
-
